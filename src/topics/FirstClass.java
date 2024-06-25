26f23caae0bb3dc8f1c4f2a27cb19139b4488520
krishna-26-6 <thondurusaikrishna54@gmail.com>
2024-06-25 16:45:02 +0530

    second commit

---
+++ src/topics/FirstClass.java
@@ -0,0 +1,11 @@
+package topics;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("this is first class");
+	}
+
+}
